a8199bfcabf128ba4094ccfeb3b80a5cda87b044
Jibon Biswas <jb.jiboninfo1994@gmail.com>
2024-09-04 17:12:19 +0600

    Note ticking app is donewith class-2-video-7-8

--- src/App.js
+++ src/App.js
@@ -2,34 +2,90 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const increaseCounter = (byCount) => {
-    setCounter(counter + byCount.payload);
+  const [noteTitle, setNoteTitle] = useState('');
+  const [notes, setNotes] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editableNote, setEditableNote] = useState(null);
+
+  // Submit handler
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!noteTitle) {
+      return alert('Note title field is required!');
+    }
+    editMode ? updateHandler() : createHandler();
   };
-  const decreaseCounter = (byCount) => {
-    setCounter(counter - byCount.payload);
+
+  // Create handler
+  const createHandler = () => {
+    const newNote = {
+      id: Date.now().toString(),
+      title: noteTitle
+    };
+
+    setNotes([...notes, newNote]);
+    setNoteTitle('');
   };
+
+  // Delete handler
+  const deleteHandler = (id) => {
+    setNotes(notes?.filter((item) => item.id !== id));
+  };
+
+  // Edit handler
+  const editHandler = (note) => {
+    setEditMode(true);
+    setEditableNote(note);
+    setNoteTitle(note.title);
+  };
+
+  // Update Handler
+  const updateHandler = () => {
+    console.log('I am update');
+    setNotes(
+      notes.map((item) => {
+        if (item.id === editableNote.id) {
+          return { ...item, title: noteTitle };
+        }
+
+        return item;
+      })
+    );
+    setEditMode(false);
+    setEditableNote(null);
+    setNoteTitle('');
+  };
+
   return (
-    <div className="App">
-      <div className="counter-app">
-        <h3>The value of counter {counter}</h3>
-        <button
-          className={counter === 0 ? 'disabled' : ''}
-          onClick={() => decreaseCounter({ payload: 1 })}
-          type="button"
-        >
-          Decrease by 1
-        </button>
-        <button
-          className={counter === 10 ? 'disabled' : ''}
-          type="button"
-          onClick={() => increaseCounter({ payload: 1 })}
-        >
-          Increase by 1
-        </button>
-        <button type="button" onClick={() => setCounter(0)}>
-          Reset Counter
-        </button>
+    <div className="App" style={{ marginTop: '40px' }}>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Note title..."
+          value={noteTitle}
+          onChange={(e) => setNoteTitle(e.target.value)}
+        />
+        <button type="submit">{editMode ? 'Update Note' : 'Add Note'}</button>
+      </form>
+      <div className="note-list">
+        <h2>Note List</h2>
+        {notes && notes.length < 1 && <p>You don't have any Note!</p>}
+        {notes && notes.length > 0 && (
+          <ul>
+            {notes?.map((note) => (
+              <li key={note.id}>
+                <span>{note.title}</span>
+                <button type="button" onClick={() => editHandler(note)}>
+                  Edit Note
+                </button>
+                <button type="button" onClick={() => deleteHandler(note.id)}>
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
