ff3167e614c58a9d143d194931444abffefc1b8d
Jibon Biswas <jb.jiboninfo1994@gmail.com>
2024-09-04 15:49:02 +0600

    Counter app is done withclass-2-video-6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import { bioData } from './data';
+import { useState } from 'react';
 import './App.css';
-import BioData from './components/BioData';
 
 function App() {
-  console.log(bioData);
-
+  const [counter, setCounter] = useState(0);
+  const increaseCounter = (byCount) => {
+    setCounter(counter + byCount.payload);
+  };
+  const decreaseCounter = (byCount) => {
+    setCounter(counter - byCount.payload);
+  };
   return (
     <div className="App">
-      {bioData &&
-        bioData.length > 0 &&
-        bioData?.map((item) => <BioData key={item.id} {...item} />)}
+      <div className="counter-app">
+        <h3>The value of counter {counter}</h3>
+        <button
+          className={counter === 0 ? 'disabled' : ''}
+          onClick={() => decreaseCounter({ payload: 1 })}
+          type="button"
+        >
+          Decrease by 1
+        </button>
+        <button
+          className={counter === 10 ? 'disabled' : ''}
+          type="button"
+          onClick={() => increaseCounter({ payload: 1 })}
+        >
+          Increase by 1
+        </button>
+        <button type="button" onClick={() => setCounter(0)}>
+          Reset Counter
+        </button>
+      </div>
     </div>
   );
 }
